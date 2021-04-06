c3c3c689e5f882b63390932c06c10be8e2392ba0
Nahoa <rraymond001@gmail.com>
2021-04-05 14:59:18 -1000

    Fixed up any extra styling elements and took away borders that I used for debugging/ Added comments to the css

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,19 @@
-//alert('You successfully linked your JavaScript file!');
-
-
 //This function Toggles the dropdown menu on and off when clicked
 function displayContent() {
-    document.getElementById("dropdown-content").classList.toggle("show");
+  document.getElementById("dropdown-content").classList.toggle("show");
 }
 
-
-// This function removes the dropdown menu when the user clicks on something other than Settings
-window.onclick = function(event) {
-    if (!event.target.matches('.settings-link')) {
-        var dropdowns = document.getElementsByClassName("settings-dropdown-content");
-        for ( var i=0;i< dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-
+// This function removes the dropdown menu when the user clicks on something other than the Settings anchor
+window.onclick = function (event) {
+  if (!event.target.matches(".settings-link")) {
+    var dropdowns = document.getElementsByClassName(
+      "settings-dropdown-content"
+    );
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
-}
+  }
+};
